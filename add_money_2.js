cbc38b9391f5c25177216b275c0b09530eb36898
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-01 01:55:59 +0800

    Update project

--- add_money_2.js
+++ add_money_2.js
@@ -1,6 +1,6 @@
 document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        const sum = getElementValueById();
-        console.log(sum);
+        const sum = getElementValueById("add_money_amount");
+
     });
