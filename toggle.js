67e5e57b4b3268cfc46cd693b909f026658b63ba
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-02 01:05:48 +0800

    Update project

--- toggle.js
+++ toggle.js
@@ -1,15 +1,18 @@
 document.getElementById('cashout-section').style.display = "none";
 
 document.getElementById('add-money_box')
-    .addEventListener('clicked', function () {
-        handleToggle("first_login_section", "none")
-        handleToggle("cashout-section", "block")
+    .addEventListener('click', function () {
+        console.log('clicked');
+        handleToggle("first_login_section", "block")
+        handleToggle("cashout-section", "none")
     });
 
 document.getElementById('cashout_box') 
-.addEventListener('clicked', function(){
+.addEventListener('click', function(){
 
       handleToggle("first_login_section", "none")
         handleToggle("cashout-section", "block")
 });
-function handleToggle
+function handleToggle(id, status){
+    document.getElementById(id).style.display = status;
+}
