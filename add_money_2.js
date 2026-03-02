7210d66daf951919b4b4de7525947e6f58597c25
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-02 14:46:19 +0800

    Update project

--- add_money_2.js
+++ add_money_2.js
@@ -1,10 +1,17 @@
+
+
+handleToggle("transection_hestory", "none");
+
 document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
+
+
         const amount = getInputValueById("add_money_amount");
         const pin = getInputValueById("add_money_pin");
         const balance = getInnerTextById("add_money_min_balence");
         const account = document.getElementById("add-money_number").value;
+
 
 
         if (account.length === 11) {
--- toggle.js
+++ toggle.js
@@ -1,18 +1,22 @@
 document.getElementById('cashout-section').style.display = "none";
+document.getElementById('first_login_section').style.display = "none";
+document.getElementById('transection_hestory').style.display = "none";
 
 document.getElementById('add-money_box')
     .addEventListener('click', function () {
         console.log('clicked');
         handleToggle("first_login_section", "block")
         handleToggle("cashout-section", "none")
+        handleToggle("transection_hestory", "none");
     });
 
-document.getElementById('cashout_box') 
-.addEventListener('click', function(){
+document.getElementById('cashout_box')
+    .addEventListener('click', function () {
 
-      handleToggle("first_login_section", "none")
+        handleToggle("first_login_section", "none")
         handleToggle("cashout-section", "block")
-});
-function handleToggle(id, status){
+        handleToggle("transection_hestory", "none");
+    });
+function handleToggle(id, status) {
     document.getElementById(id).style.display = status;
 }
