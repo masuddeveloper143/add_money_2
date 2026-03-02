7a6b1c4e176a713e7944dfcd122ac95a91ca80d5
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-03 00:15:47 +0800

    Update project

--- add_money_2.js
+++ add_money_2.js
@@ -8,29 +8,34 @@ document.getElementById('btn-add-money')
         const balance = getInnerTextById("add_money_min_balence");
         const account = document.getElementById("add-money_number").value;
 
-        
-
-        if (account.length === 11) {
-            if (pin === 1234) {
-                const sum = balance + amount;
-                // document.getElementById("add_money_min_balence").innerText = sum;
-                setInnerTextByIdandValue("add_money_min_balence", sum);
-                // console.log(sum);
-                 console.log('add money successful🎉');
-
-
-                const container = document.getElementById('transaction-container');
-                const p = document.createElement("p");
-
-                p.innerText = `added ${amount} from ${account} acount`;
-                container.appendChild(p)
-
-
-            } else {
-                alert('incorrect pin')
-            }
-        } else {
-            alert('invalid number');
+        if (amount < 0) {
+            alert("added money minimum 1 tk")
+            return;
         }
+        if (amount > balance){
+            alert("invalig")
+        }
+            if (account.length === 11) {
+                if (pin === 1234) {
+                    const sum = balance + amount;
+                    // document.getElementById("add_money_min_balence").innerText = sum;
+                    setInnerTextByIdandValue("add_money_min_balence", sum);
+                    // console.log(sum);
+                    console.log('add money successful🎉');
+
+
+                    const container = document.getElementById('transaction-container');
+                    const p = document.createElement("p");
+
+                    p.innerText = `added ${amount} from ${account} acount`;
+                    container.appendChild(p)
+
+
+                } else {
+                    alert('incorrect pin')
+                }
+            } else {
+                alert('invalid number');
+            }
 
     });
