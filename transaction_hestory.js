0d499ee2b0bb05b100886b4270e3bad3c0ffb23b
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-02 14:36:22 +0800

    Update project

---
+++ transaction_hestory.js
@@ -0,0 +1,6 @@
+document.getElementById('transaction_box')
+.addEventListener('click', function () {
+    handleToggle("cashout-section", "none");
+    handleToggle("first_login_section", "none")
+
+})
